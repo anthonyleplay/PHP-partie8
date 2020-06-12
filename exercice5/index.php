2ac48f525e3758faff2786c027f0e6bc13c30862
Anthony Le Play <anthony.leplay@gmail.com>
2020-06-12 14:39:53 +0200

    4et5

--- exercice5/index.php
+++ exercice5/index.php
@@ -1,19 +1,34 @@
 <?php
-    if (isset($_GET['firstname'])) {
-        $messageFirstname = $_GET['firstname'];
+
+$message = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $loginName = 'login';
+    $passName = 'password';
+    $passValue = $_POST['password'];
+
+    if (empty($_POST[$loginName])) {
+        $message = "merci d'entré un Login et un Mot de passe";
     } else {
-        $messageFirstname = 'il n\'y a pas de parametre d\'URL \'firstname\'';
+        $loginValue = $_POST[$loginName];
+
+        if (empty($_POST[$passName])) {
+            $message = "merci d'entré un Login et un Mot de passe";
+        } else {
+            $passValue = $_POST[$passName];
+            setcookie($loginName, $loginValue, time() + (86400 * 30), "/"); // 86400 = 1 day
+            setcookie($passName, $passValue, time() + (86400 * 30), "/"); // 86400 = 1 day
+
+            if (!isset($_COOKIE[$loginName]) && !isset($_COOKIE[$passName])) {
+                $message = "merci d'entré un Login et un Mot de passe";
+            } else {
+                $message = "votre login est : " . $_COOKIE[$loginName] . "<br>
+                            Votre mot de passe est : " . $_COOKIE[$passName];
+            };
+        };
     };
-    if (isset($_GET['lastname'])) {
-        $messageLastname = $_GET['lastname'];
-    } else {
-        $messageLastname = 'il n\'y a pas de parametre d\'URL \'lastname\'';
-    };
-    if (isset($_GET['gender'])) {
-        $messageGender = $_GET['gender'];
-    } else {
-        $messageGender = 'il n\'y a pas de parametre d\'URL \'gender\'';
-    };
+};
+
 ?>
 
 <!DOCTYPE html>
@@ -27,36 +42,26 @@
 
 <body>
     <h1>Exercice 5</h1>
+    <h2>page1</h2>
 
-    <p>Créer un formulaire sur la page index.php avec : <br>
-        <ul>
-            <li>Une liste déroulante pour la civilité (Mr ou Mme)</li>
-            <li>Un champ texte pour le nom</li>
-            <li>Un champ texte pour le prénom </li>
-        </ul><br>
-        Ce formulaire doit rediriger vers la page index.php.<br>
-        Vous avez le choix de la méthode.
-    </p>
-    <a href="index.php"><span style="background-color: lightblue;">Reset</span></a>
+    <p>Faire une page qui va récupérer les informations du cookie créé à l'exercice 3 et qui les affiche.</p>
+    <p>(EXO3 : Faire un formulaire qui permet de récupérer le login et le mot de passe de l'utilisateur. <br>
+        A la validation du formulaire, stocker les informations dans un cookie.)</p>
+
+    
+    <a href="index.php"><span style="background-color: lightcoral;">reset</span></a><br><br><br>
+    <a href="page2.php"><span style="background-color: lightblue;">lien vers page2</span></a><br><br>
 
     <p>===================================</p>
 
-    <form action="/exercice5/index.php" method=get>
-        <label for="gender">civilité:</label>
-        <select id="gender" name="gender">
-            <option value="Homme">Mr</option>
-            <option value="Femme">Mme</option>
-        </select><br>
-        <label for="firstname">Prenom:</label><br>
-        <input type="text" id="firstname" name="firstname" value="Aude" require><br>
-        <label for="lastname">Nom:</label><br>
-        <input type="text" id="lastname" name="lastname" value="Vessel" require><br><br>
+    <form action="/exercice5/page2.php" method="post">
+        <label for="login">login:</label><br>
+        <input type="text" id="login" name="login" required><br>
+        <label for="password">Mot de Passe:</label><br>
+        <input type="password" id="password" name="password" required><br><br>
         <input type="submit" value="envoyer">
-    </form>
+    </form><br><br>
 
-    <p><?= 'civilité : ' . $messageGender; ?> </p>
-    <p><?= 'Prenom : ' . $messageFirstname; ?> </p>
-    <p><?= 'Nom : ' . $messageLastname; ?> </p>
 
 </body>
 
--- exercice5/page2.php
+++ exercice5/page2.php
@@ -37,31 +37,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>exo4</title>
+    <title>exo5</title>
 </head>
 
 <body>
-    <h1>Exercice 4</h1>
-    <h2>page1</h2>
+    <h1>Exercice 5</h1>
+    <h2>page2</h2>
 
     <p>Faire une page qui va récupérer les informations du cookie créé à l'exercice 3 et qui les affiche.</p>
     <p>(EXO3 : Faire un formulaire qui permet de récupérer le login et le mot de passe de l'utilisateur. <br>
         A la validation du formulaire, stocker les informations dans un cookie.)</p>
 
     
-    <a href="index.php"><span style="background-color: lightcoral;">reset</span></a><br><br><br>
-    <a href="page2.php"><span style="background-color: lightblue;">lien vers page2</span></a><br><br>
+    <a href="page2.php"><span style="background-color: lightcoral;">reset</span></a><br><br><br>
+    <a href="index.php"><span style="background-color: lightgreen;">lien vers page1</span></a><br><br>
 
     <p>===================================</p>
 
-    <form action="/exercice4/page2.php" method="post">
-        <label for="login">login:</label><br>
-        <input type="text" id="login" name="login" required><br>
-        <label for="password">Mot de Passe:</label><br>
+    <form action="/exercice5/page2.php" method="post">
+        <label for="password"> modif Mot de Passe:</label><br>
         <input type="password" id="password" name="password" required><br><br>
         <input type="submit" value="envoyer">
     </form><br><br>
 
+    <p><?= $message ?></p>
 
 </body>
 
